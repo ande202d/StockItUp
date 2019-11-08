1c2d67641a159998628e05cdc2f2a1ebc2da4e2e
Anders Garbacz Hansen <ande202d@edu.easj.dk>
2019-11-08 09:22:38 +0100

    first commit

---
+++ StockItUp/FirstTestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockItUp
+{
+    class FirstTestClass
+    {
+        //det er bare for at tjekke vi alle har det samme
+    }
+}
