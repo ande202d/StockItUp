d217490d6b5afb7d73997272e37a5b4a3fdc14cd
Timm Oleander Nielsen <timm0192@edu.easj.dk>
2019-11-12 09:03:04 +0100

    GUI og AppBarButtonBehavior

--- StockItUp/View/Lager.xaml.cs
+++ StockItUp/View/Lager.xaml.cs
@@ -15,14 +15,14 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace StockItUp
+namespace StockItUp.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class Lager : Page
     {
-        public BlankPage1()
+        public Lager()
         {
             this.InitializeComponent();
         }
--- StockItUp/View/Personale.xaml.cs
+++ StockItUp/View/Personale.xaml.cs
@@ -13,23 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace StockItUp
+namespace StockItUp.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Personale : Page
     {
-        public MainPage()
+        public Personale()
         {
             this.InitializeComponent();
-        }
-
-        private void AppBarButton_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
