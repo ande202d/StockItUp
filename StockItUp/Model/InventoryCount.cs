a6f68a7d8a9aea801ed0ee6b31e8b00149c69701
Anders Garbacz Hansen <ande202d@edu.easj.dk>
2019-11-14 10:45:58 +0100

    added InventoryCount and unittest there

---
+++ StockItUp/Model/InventoryCount.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockItUp.Model
+{
+    public class InventoryCount
+    {
+        private int _id;
+        private static int _idCounter = 1;
+        private DateTime _dateTime;
+        private Dictionary<Product, int> _counting;
+        private Location _location;
+
+        #region Constructor
+        public InventoryCount(Location location, Dictionary<Product, int> dictionary)
+        {
+            _id = _idCounter;
+            _idCounter++;
+            _dateTime = DateTime.Now;
+            _location = location;
+            _counting = dictionary;
+        }
+        #endregion
+
+        #region Properties
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public DateTime DateTime
+        {
+            get { return _dateTime; }
+            set { _dateTime = value; }
+        }
+        public Location Location
+        {
+            get { return _location; }
+            set { _location = value; }
+        }
+        public Dictionary<Product, int> GetDictionary
+        {
+            get { return _counting; }
+            set { _counting = value; }
+        }
+        public List<Product> ListProducts
+        {
+            get { return _counting.Keys.ToList(); }
+        }
+        public List<int> ListAmounts
+        {
+            get { return _counting.Values.ToList(); }
+        }
+        #endregion
+    }
+}
---
+++ UnitTestStockItUp/InventoryCountUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using StockItUp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StockItUp.Model;
+
+namespace UnitTestStockItUp
+{
+    [TestClass]
+    public class InventoryCountUnitTest
+    {
+        [TestMethod]
+        public void TestConstructor_Id()
+        {
+            //Arrange
+            InventoryCount ic1;
+            InventoryCount ic2;
+            InventoryCount ic3;
+            InventoryCount ic4;
+            InventoryCount ic5;
+
+            //Act
+            ic1 = new InventoryCount((new Location(new Store("store1", "adress1"), "location1")), new Dictionary<Product, int>());
+        
+            //Assert
+            //Assert.AreEqual(s2.Id+1, s3.Id, "Id dosent add up automaticly");
+        }
+    }
+}
