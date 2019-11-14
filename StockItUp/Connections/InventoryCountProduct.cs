d9d823eb4b61af74255f0c530d0d2f64ae4efe03
Anders Garbacz Hansen <ande202d@edu.easj.dk>
2019-11-14 12:24:47 +0100

    connections

---
+++ StockItUp/Connections/InventoryCountProduct.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockItUp.Connections
+{
+    public class InventoryCountProduct
+    {
+        private int _storeId;
+        private int _productId;
+        private int _amount;
+
+        public InventoryCountProduct(int storeId, int productId, int amount)
+        {
+            _storeId = storeId;
+            _productId = productId;
+            _amount = amount;
+        }
+
+        public int StoreId
+        {
+            get { return _storeId; }
+            set { _storeId = value; }
+        }
+        public int ProductId
+        {
+            get { return _productId; }
+            set { _productId = value; }
+        }
+        public int Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+    }
+}
