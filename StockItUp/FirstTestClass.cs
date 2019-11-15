4edcee3828fdf73a71dcc9b77a0d3a9fdfacb565
Jonas Andersen <jona32d8@edu.easj.dk>
2019-11-15 11:28:36 +0100

    Fået listview til at fungere samt opdateret gui med 'Administrer varer' og 'Administrer leverandør' samt knapper og felter til test.

--- StockItUp/FirstTestClass.cs
+++ StockItUp/FirstTestClass.cs
@@ -14,11 +14,11 @@ namespace StockItUp
         private ObservableCollection<Product> _test;
         public FirstTestClass()
         {
-            Product p1 = new Product("1", 1);
-            Product p2 = new Product("2", 2, new Supplier("s1", "www.hejmeddig.dk"));
-            Product p3 = new Product("3", 3);
-            Product p4 = new Product("4", 4, new Supplier("s2", "www.hejmeddigv2.dk"));
-            Product p5 = new Product("5", 5);
+            Product p1 = new Product("Classic", 24, new Supplier("s1", "www.tuborg.dk"));
+            Product p2 = new Product("Stolichnaya", 6, new Supplier("s2", "www.Stolichnaya.com"));
+            Product p3 = new Product("Breezer", 24, new Supplier("s3", "www.breezer.dk"));
+            Product p4 = new Product("Mokai", 24, new Supplier("s4", "www.mokaaai.dk"));
+            Product p5 = new Product("Brugal Anejo", 6, new Supplier("s5", "www.BrugalAnejo.dk"));
             _test = new ObservableCollection<Product>(){p1,p2,p3,p4,p5};
         
         }
