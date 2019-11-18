a64010958bd1c8d25d9632dd3a19ff2d8b0a797b
Timm Oleander Nielsen <timm0192@edu.easj.dk>
2019-11-18 10:29:10 +0100

    Db

---
+++ StockItUpDatabase/dbo/Tables/StoreProduct.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[StoreProduct] (
+    [Store]   INT NOT NULL,
+    [Product] INT NOT NULL,
+    [Amount]  INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Store] ASC, [Product] ASC),
+    CONSTRAINT [FK_StoreProduct_Procut] FOREIGN KEY ([Product]) REFERENCES [dbo].[Product] ([Id]),
+    CONSTRAINT [FK_StoreProduct_ToTable] FOREIGN KEY ([Store]) REFERENCES [dbo].[Store] ([Id])
+);
+
---
+++ StockItUpDatabase/dbo/Tables/Supplier.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Supplier] (
+    [Id]      INT          IDENTITY (1, 1) NOT NULL,
+    [Name]    VARCHAR (50) NOT NULL,
+    [Website] VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
