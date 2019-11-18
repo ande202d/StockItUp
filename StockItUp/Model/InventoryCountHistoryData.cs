23874f37798081857c86e3fcf7439d2b25634614
Anders Garbacz Hansen <ande202d@edu.easj.dk>
2019-11-18 10:16:56 +0100

    added history

---
+++ StockItUp/Model/InventoryCountHistoryData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockItUp.Model
+{
+    public class InventoryCountHistoryData
+    {
+        private int _id; //the id from the inventoryCountHistory witch is also the same as the InventoryCount
+        private string _product;
+        private int _amount;
+
+        //Something simular the inventoryCountHistory
+        //where it takes a InventoryCount, and then finds the matching inventoryCountProducts
+        public InventoryCountHistoryData(int id, string product, int amount)
+        {
+            _id = id;
+            _product = product;
+            _amount = amount;
+        }
+
+        #region Properties
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Product1
+        {
+            get { return _product; }
+            set { _product = value; }
+        }
+
+        public int Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        } 
+        #endregion
+
+    }
+}
---
+++ StockItUp/Model/OrderHistory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockItUp.Model
+{
+    public class OrderHistory
+    {
+        private int _id;
+        private DateTime _orderedDate;
+
+        //same comment as the one in inventoryCountHistory
+        public OrderHistory()
+        {
+            //Todo
+        }
+
+        #region properties
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public DateTime OrderedDate
+        {
+            get { return _orderedDate; }
+            set { _orderedDate = value; }
+        } 
+        #endregion
+
+    }
+}
