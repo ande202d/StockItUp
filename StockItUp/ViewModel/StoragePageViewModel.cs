d3aca38d9f511b1f3a79ec347ba89536d7dbd778
Timm Oleander Nielsen <timm0192@edu.easj.dk>
2019-11-19 12:58:03 +0100

    First version of StoragePageVievModel, and also made some changes for the Storage xaml view

---
+++ StockItUp/ViewModel/StoragePageViewModel.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using RVG.Common;
+using StockItUp.Annotations;
+using StockItUp.Model;
+using StockItUp.Persistency;
+
+namespace StockItUp.ViewModel
+{
+    public class StoragePageViewModel : INotifyPropertyChanged
+    {
+        #region Instance fields
+
+        private Catalog<Location> _locationCatalog;
+        private Catalog<Store> _storeCatalog;
+        private Location _selectedLocation;
+        private Store _selectedStore;
+
+        #endregion
+
+        #region Constructor
+
+        public StoragePageViewModel()
+        {
+            _locationCatalog = Catalog<Location>.Instance;
+            _storeCatalog = Catalog<Store>.Instance;
+            CreateLocationCommand = new RelayCommand(CreateLocationMethod);
+            DeleteLocationCommand = new RelayCommand(DeleteLocationMethod);
+            UpdateLocationCommand = new RelayCommand(UpdateLocationMethod);
+
+        }
+
+        #endregion
+
+        #region Commands
+        public ICommand CreateLocationCommand { get; set; }
+        public ICommand DeleteLocationCommand { get; set; }
+        public ICommand UpdateLocationCommand { get; set; }
+
+
+
+        #endregion
+
+  
+
+        #region Porperties
+
+        //returns an observable of products collection based on what the catalog pulls from the database
+        public ObservableCollection<Location> LocationCatalog
+        {
+            get
+            {
+                ObservableCollection<Location> collection = new ObservableCollection<Location>(_locationCatalog.ReadAll().Result);
+
+
+                return collection;
+            }
+        }
+
+        public ObservableCollection<Store> StoreCatalog
+        {
+            get
+            {
+                ObservableCollection<Store> collection = new ObservableCollection<Store>(_storeCatalog.ReadAll().Result);
+
+                return collection;
+            }
+        }
+
+
+        public Location SelectedLocation
+        {
+            get { return _selectedLocation; }
+            set { _selectedLocation = value; OnPropertyChanged(); }
+        }
+
+        public Store SelectedStore
+        {
+            get { return _selectedStore; }
+            set { _selectedStore = value; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void CreateLocationMethod()
+        {
+           // _locationCatalog.Create(new Location());
+        }
+
+ 
+
+        private void DeleteLocationMethod()
+        {
+            _locationCatalog.Delete(SelectedLocation.Id);
+        }
+
+
+        private void UpdateLocationMethod()
+        {
+            _locationCatalog.update(SelectedLocation.Id, SelectedLocation);
+        }
+
+
+
+
+        #endregion
+
+        #region OnPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+    }
+}
