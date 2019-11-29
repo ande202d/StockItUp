19cad9094d9e0456e849b66123fe00e3ac09eb49
Anders Garbacz Hansen <ande202d@edu.easj.dk>
2019-11-29 10:33:51 +0100

    fixede bug med optællinger

--- StockItUp/ViewModel/InventoryCountViewModel.cs
+++ StockItUp/ViewModel/InventoryCountViewModel.cs
@@ -42,7 +42,6 @@ namespace StockItUp.ViewModel
             _inventoryCountProductCatalog = Catalog<InventoryCountProduct>.Instance;
             _inventoryCountCatalog = Catalog<InventoryCount>.Instance;
             CreateInventoryCountCommand = new RelayCommand(CreateInventoryCountMethod);
-            SelectedInventoryCountHistory = new InventoryCountHistory(new InventoryCount(new Location(Catalog<Store>.Instance.Read(1).Result, default(string))));
         }
 
         #endregion
@@ -125,7 +124,11 @@ namespace StockItUp.ViewModel
         //To select a different old inventoryCount to show data from
         public InventoryCountHistory SelectedInventoryCountHistory
         {
-            get { return _selectedInventoryCountHistory; }
+            get
+            {
+                if (_selectedInventoryCountHistory == null) return new InventoryCountHistory();
+                return _selectedInventoryCountHistory;
+            }
             set { _selectedInventoryCountHistory = value; OnPropertyChanged();
                 OnPropertyChanged(nameof(InventoryCountHistoriesCatalogData));
             }
