bcd62441f615cf9fe6bc532d81bc9b310be5d68c
Anders Garbacz Hansen <ande202d@edu.easj.dk>
2019-12-09 11:22:55 +0100

    added classes

---
+++ StockItUp/Connections/UserStore.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockItUp.Connections
+{
+    public class UserStore
+    {
+        private int _id;
+        private static int _idCounter = 1;
+        private int _userId;
+        private int _storeId;
+
+        public UserStore(int userId, int storeId)
+        {
+            _id = _idCounter;
+            _idCounter++;
+
+            _userId = userId;
+            _storeId = storeId;
+        }
+        public UserStore()
+        {
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public int UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+
+        public int StoreId
+        {
+            get { return _storeId; }
+            set { _storeId = value; }
+        }
+    }
+}
---
+++ StockItUp/Persistency/Controller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockItUp.Persistency
+{
+    public class Controller
+    {
+        private int _userId;
+        private int _storeId;
+
+        #region Singleton
+        private static Controller _instance;
+
+        public static Controller Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Controller();
+                }
+                return _instance;
+            }
+        }
+        #endregion
+
+        public int UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+
+        /*
+        public User GetUser
+        {
+            get {return Catalog<User>.Instance.read(UserId).Result;}
+        }
+        */
+
+        public int StoreId
+        {
+            get { return _storeId; }
+            set { _storeId = value; }
+        }
+
+    }
+}
