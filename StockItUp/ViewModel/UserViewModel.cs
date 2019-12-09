c535ddbe8f371eb5b1cf479c7cf108cef6e8559e
Anders Garbacz Hansen <ande202d@edu.easj.dk>
2019-12-09 13:15:57 +0100

    userViewModel

---
+++ StockItUp/ViewModel/UserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockItUp.Persistency;
+
+namespace StockItUp.ViewModel
+{
+    public class UserViewModel
+    {
+
+
+        public Controller GetController
+        {
+            get { return Controller.Instance;}
+        }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+
+        public void ChangePasswordMethod()
+        {
+            if (OldPassword == GetController.GetUser.Password && !String.IsNullOrWhiteSpace(NewPassword))
+            {
+                GetController.GetUser.Password = NewPassword;
+                OldPassword = default(string);
+                NewPassword = default(string);
+            }
+        }
+    }
+}
